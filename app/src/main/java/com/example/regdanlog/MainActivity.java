b0e4c79861267a916ac40ff9f9a727b2b8ac2fae
admin hay <hanifahayuy@gmail.com>
2024-08-08 00:04:44 +0700

    buat reg dan log tahap 2

--- app/src/main/java/com/example/regdanlog/MainActivity.java
+++ app/src/main/java/com/example/regdanlog/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler() .postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this,Login.class);
+                Intent intent = new Intent(MainActivity.this,Home.class);
                 startActivity(intent);
                 finish();
             }
