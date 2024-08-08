79bc9d604c34cae4668619420564bbb4d6373253
admin hay <hanifahayuy@gmail.com>
2024-08-08 08:39:48 +0700

    buat reg dan log tahap 3

--- app/src/main/java/com/example/regdanlog/DatabaseHelper.java
+++ app/src/main/java/com/example/regdanlog/DatabaseHelper.java
@@ -18,19 +18,19 @@ public class DatabaseHelper {
     }
 
     public  void onCreat(SQLiteDatabase db) {
-        db.execSQL("create table" + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, EMAIL TEXT, PASSWORD TEXT)");
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, EMAIL TEXT, PASSWORD TEXT)");
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXIST" + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreat(db);
     }
 
     public boolean insert(String email, String password) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        ContentValues.put(COL_2, email);
-        ContentValues.put(COL_3, password);
+        ContentValues.put (COL_2, email);
+        ContentValues.put (COL_3, password);
         long result = db.insert(TABLE_NAME, null, contentValues);
         return result != -1;
     }
@@ -41,14 +41,14 @@ public class DatabaseHelper {
 
     public boolean checkEmail(String email){
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM" + TABLE_NAME + "WHERE EMAIL = ?", new String[]{email});
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE EMAIL = ?", new String[]{email});
         if (cursor.getCount() > 0) return  true;
         else return false;
     }
 
     public boolean checkEmailPassword(String email, String password) {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM" + TABLE_NAME + "WHERE EMAIL = ? AND PASSWORD = ?", new String[]{email, password});
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE EMAIL = ? AND PASSWORD = ?", new String[]{email, password});
         if (cursor.getCount() > 0) return true;
         else return false;
     }
